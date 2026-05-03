899bf4abc5c78528c94b30f7ff80364f804d2f43
farisan <muhammad.farisan99@gmail.com>
2026-05-03 16:44:30 +0700

    feat: implement centralized API service with axios and authentication interceptors

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API_URL = 'http://localhost:5000/api';
-const API_URL = process.env.REACT_APP_API;
+const API_URL = 'https://be-management-padi.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_URL,
